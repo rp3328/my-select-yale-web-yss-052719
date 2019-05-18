7eac4740acd650474b5587b603c57aa09e7f9c6c
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-18 11:16:51 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
+ num = 0
+ clen = collection.size
+ arr = []
+
+ while num < clen
+   if yield(collection[num]) == true
+     arr << collection[num]
+   end
+     num += 1
+   end
+   arr
 end
